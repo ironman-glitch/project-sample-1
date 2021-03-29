f3537745706363ca19f56f927010ff14fef6ffb4
ironman <66557870+ironman-glitch@users.noreply.github.com>
2021-03-29 21:20:43 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,3 +15,28 @@ basic.forever(function () {
         strip.showRainbow(720, 900)
     }
 })
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        status = 1
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        status = 1
+    } else if (pins.digitalReadPin(DigitalPin.P11) == 1) {
+        status = 1
+    } else {
+        status = 1
+    }
+    if ((status == 1 && pins.digitalReadPin(DigitalPin.P2) == 1 || pins.digitalReadPin(DigitalPin.P11) == 1 && false) && pins.digitalReadPin(DigitalPin.P11) == 0) {
+        basic.showIcon(IconNames.Heart)
+        cont = 1
+    } else if ((status || false) && pins.digitalReadPin(DigitalPin.P2) == 1 && cont == 1) {
+        for (let index = 0; index < 5; index++) {
+            basic.showIcon(IconNames.Target)
+            basic.pause(2000)
+        }
+        basic.clearScreen()
+        cont = 0
+    } else {
+        basic.clearScreen()
+    }
+    basic.pause(200)
+})
