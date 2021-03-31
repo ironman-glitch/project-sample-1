ae70e7bc34abba8ee4558f608c083a075756579b
ironman <66557870+ironman-glitch@users.noreply.github.com>
2021-03-31 15:11:48 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,36 @@
-let strip = neopixel.create(DigitalPin.P2, 4, NeoPixelMode.RGB)
-let status = 0
-let cont = 0
+let count = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     if (pins.digitalReadPin(DigitalPin.P11) == 1) {
-    	
-    }
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
-        strip.showRainbow(1, 360)
-        basic.pause(100)
-        strip.showRainbow(360, 720)
-        basic.pause(100)
-        strip.showRainbow(720, 900)
-    }
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        status = 1
-    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        status = 1
-    } else if (pins.digitalReadPin(DigitalPin.P11) == 1) {
-        status = 1
-    } else {
-        status = 1
-    }
-    if ((status == 1 && pins.digitalReadPin(DigitalPin.P2) == 1 || pins.digitalReadPin(DigitalPin.P11) == 1 && false) && pins.digitalReadPin(DigitalPin.P11) == 0) {
-        basic.showIcon(IconNames.Heart)
-        cont = 1
-    } else if ((status || false) && pins.digitalReadPin(DigitalPin.P2) == 1 && cont == 1) {
-        for (let index = 0; index < 5; index++) {
-            basic.showIcon(IconNames.Target)
-            basic.pause(2000)
+        count = 0
+        while (count < 600) {
+            basic.pause(100)
+            count += 1
+            if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                count = 600
+                while (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . # . .
+                        . # . . .
+                        . . . # .
+                        . . . . .
+                        `)
+                }
+            }
         }
-        basic.clearScreen()
-        cont = 0
-    } else {
-        basic.clearScreen()
     }
-    basic.pause(200)
 })
